6cde0ce1cce7dd932c739182b87dd73c5fa9acbe
FukeKazki <kazkichi0906@gmail.com>
2020-08-29 18:04:40 +0900

    Fix

--- ls_4432.c
+++ ls_4432.c
@@ -4,19 +4,17 @@
 #define REP(i, n) for (int i = 0; i < (n); ++i)
 #define REPR(i, n) for (int i = n; i >= 0; --i)
 #define FOR(i, n, m) for (int i = m; i < n; i++)
+#define FN 2
 
-void ffv(int p, double a, double *b) {
+double ffv(int p, double a) {
     switch (p)
     {
+    case 0:
+        return 1.0;
     case 1:
-        *b = a;
-        break;
-    case 2:
-        *b = 1.0;
-        break;
+        return a;
     default:
-        *b = a;
-        break;
+        return 1.0;
     }
 }
 
@@ -30,8 +28,9 @@ int main(void) {
     printf("\ty = a*e^bx\n");
     printf("の形の曲線をあてはめるものです。\n\n");
 
-    f = 1;
-    g = 2;
+    // 基本関数の指定
+    f = 0;
+    g = 1;
 
     while(1) {
         printf("データの個数は何個ですか？ (1<n<10) n = ");
@@ -45,12 +44,14 @@ int main(void) {
             printf("\tY = ");
             scanf("%lf%c", &y, &zz);
 
-            ffv(f, x, &fx);
-            ffv(g, x, &gx);
-
-            a[i][0] = fx;
-            a[i][1] = gx;
+            a[i][0] = ffv(f, x);
+            a[i][1] = ffv(g, x);
             a[i][2] = logf(y);
+            /*
+            | f1(x1) f2(x1) Y1|
+            | f1(x2) f2(x2) Y2| 
+            | f1(x3) f2(x3) Y3|
+            */
         }
 
         printf("\n");
@@ -60,34 +61,59 @@ int main(void) {
     }
 
     printf("\n");
+    puts("入力された行列");
+    REP(i, n) {
+        REP(j, 3) {
+            printf("%lf ", a[i][j]);
+        }
+        puts("");
+    }
+    printf("\n");
+    // 拡大係数行列 tA[A b] の計算
+    REP(i, FN) {
+        REP(j, FN+1) {
+            c[i][j] = 0.0;
+            REP(k, n) {
+                c[i][j] += a[k][i] * a[k][j];
+            }
+        }
+    }
+
+    puts("拡大係数行列 tA[A b]の結果");
 
     REP(i, 2) {
         REP(j, 3) {
-            s = 0.0;
-            REP(l, n) {
-                s = s + a[l][i] * a[l][j];
+            printf("%lf ", c[i][j]);
+        }
+        puts("");
+    }
+    // c[2][3]
+
+    REP(i, FN) {
+        double p = c[i][i];
+        FOR(j, FN+1, i) {
+            c[i][j] = c[i][j] / p;
+        }
+        REP(k, FN) {
+            if (k == i) continue;
+            double q = c[k][i];
+            FOR(j, FN+1, i) {
+                c[k][j] -= q*c[i][j];
             }
-            c[i][j] = s;
         }
     }
 
+    puts("ガウスジョルダンの結果");
+
     REP(i, 2) {
-        double p = c[i][i];
-        FOR(j, 3, i) {
-            c[i][j] = c[i][j] / p;
+        REP(j, 3) {
+            printf("%lf ", c[i][j]);
         }
-        REP(l, 2) {
-            if (l != i) {
-                double q = c[l][i];
-                FOR(j, 3, i) {
-                    c[l][j] = c[l][j] - q*c[i][j];
-                }
-            }
-        }
+        puts("");
     }
 
     printf("xとyの関数関係は,\n");
-    printf("y = %lf*e^%lfx\n", exp(c[1][2]), c[0][2]);
+    printf("y = %lf*e^%lfx\n", exp(c[0][2]), c[1][2]);
     printf("です。");
     
 
